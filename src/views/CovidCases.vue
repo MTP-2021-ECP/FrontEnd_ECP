07dfaebf5cc6acd0c9781864750b8a540d786da2
Slambert <sharif.lewis-lambert@epitch.eu>
2021-01-21 12:31:20 +0100

    Created CovidCases page with stats component & routes

---
+++ src/views/CovidCases.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Stats />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Stats from "@/components/Stats.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Stats,
+  },
+};
+</script>
