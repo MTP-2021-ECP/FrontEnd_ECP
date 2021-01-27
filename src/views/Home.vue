6e750e563c8af49018d7bf20bfaf143b29341724
Slambert <sharif.lewis-lambert@epitch.eu>
2021-01-27 10:18:16 +0100

    added chatbot to home 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,30 +15,26 @@ export default {
   },
 
   created() {
-    !(function(t, a) {
-      var e = function() {
+    console.log(window)
+    console.log(document)
+    
+    
+      let e = () => {
+        
         (e.q = e.q || []).push(arguments);
       };
-      (e.q = e.q || []), (t.BotStarApi = e);
-      var n = function() {
-        var t = a.createElement("script");
-        (t.type = "text/javascript"),
-          (t.async = !0),
-          (t.src = "https://widget.botstar.com/embed/bundle.js");
-        var e = a.getElementsByTagName("script")[0];
-        e.parentNode.insertBefore(t, e);
+      (e.q = e.q || []), (window.BotStarApi = e);
+      let n = () => {
+        let window = document.createElement("script");
+        (window.type = "text/javascript"),
+          (window.async = !0),
+          (window.src = "https://widget.botstar.com/embed/bundle.js");
+        let e = document.getElementsByTagName("script")[0];
+        e.parentNode.insertBefore(window, e);
       };
-      t.attachEvent
-        ? t.attachEvent("onload", n)
-        : t.addEventListener("load", n, !1);
-    })(window, document);
-
-    this.BotStarApi("update", {
-      user_id: "1b80c408-0e11-46f6-8b0a-7c7022338d29",
-      first_name: "Foo",
-      last_name: "Boo",
-      email: "your@user.email",
-    });
+      window.attachEvent
+        ? window.attachEvent("onload", n)
+        : window.addEventListener("load", n, !1);
   },
 };
 </script>
